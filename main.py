b94e5c0d7cc4af5de38ad5ee915d3af6aab50c84
SimpleElectronicsYT <simpleelectronicsyt@gmail.com>
2025-12-31 19:59:06 -0500

    added comments

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ def convert_distance(distance):
     mile_from_km = distance * 1.6
     km_from_mile = distance * 0.62
     print(f"{distance} miles are equal to {mile_from_km} kilometers and {distance} kilometers are equal to {km_from_mile} miles")
-
+# For the future: figure out how to get user input
 distance = 4
 convert_distance(distance)
